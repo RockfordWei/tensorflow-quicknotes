be41bfc87f3ee9189fcd16327574e122b7098cf7
Rockford Wei <rockywei@LionVille.local>
2018-06-29 13:14:41 -0400

    init

---
+++ mnist_full.py
@@ -0,0 +1,254 @@
+from __future__ import print_function
+
+import glob
+import math
+import os
+
+from IPython import display
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+mnist_dataframe = pd.read_csv(
+  "mnist_train_small.csv",
+  sep=",",
+  header=None)
+
+# Use just the first 10,000 records for training/validation.
+mnist_dataframe = mnist_dataframe.head(10000)
+
+mnist_dataframe = mnist_dataframe.reindex(np.random.permutation(mnist_dataframe.index))
+
+
+def parse_labels_and_features(dataset):
+  """Extracts labels and features.
+  
+  This is a good place to scale or transform the features if needed.
+  
+  Args:
+    dataset: A Pandas `Dataframe`, containing the label on the first column and
+      monochrome pixel values on the remaining columns, in row major order.
+  Returns:
+    A `tuple` `(labels, features)`:
+      labels: A Pandas `Series`.
+      features: A Pandas `DataFrame`.
+  """
+  labels = dataset[0]
+
+  # DataFrame.loc index ranges are inclusive at both ends.
+  features = dataset.loc[:,1:784]
+  # Scale the data to [0, 1] by dividing out the max value, 255.
+  features = features / 255
+
+  return labels, features
+
+training_targets, training_examples = parse_labels_and_features(mnist_dataframe[:7500])
+validation_targets, validation_examples = parse_labels_and_features(mnist_dataframe[7500:10000])
+
+
+def construct_feature_columns():
+  """Construct the TensorFlow Feature Columns.
+
+  Returns:
+    A set of feature columns
+  """ 
+  
+  # There are 784 pixels in each image.
+  return set([tf.feature_column.numeric_column('pixels', shape=784)])
+
+def create_training_input_fn(features, labels, batch_size, num_epochs=None, shuffle=True):
+  """A custom input_fn for sending MNIST data to the estimator for training.
+
+  Args:
+    features: The training features.
+    labels: The training labels.
+    batch_size: Batch size to use during training.
+
+  Returns:
+    A function that returns batches of training features and labels during
+    training.
+  """
+  def _input_fn(num_epochs=None, shuffle=True):
+    # Input pipelines are reset with each call to .train(). To ensure model
+    # gets a good sampling of data, even when number of steps is small, we 
+    # shuffle all the data before creating the Dataset object
+    idx = np.random.permutation(features.index)
+    raw_features = {"pixels":features.reindex(idx)}
+    raw_targets = np.array(labels[idx])
+   
+    ds = Dataset.from_tensor_slices((raw_features,raw_targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    if shuffle:
+      ds = ds.shuffle(10000)
+    
+    # Return the next batch of data.
+    feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
+    return feature_batch, label_batch
+
+  return _input_fn
+
+def create_predict_input_fn(features, labels, batch_size):
+  """A custom input_fn for sending mnist data to the estimator for predictions.
+
+  Args:
+    features: The features to base predictions on.
+    labels: The labels of the prediction examples.
+
+  Returns:
+    A function that returns features and labels for predictions.
+  """
+  def _input_fn():
+    raw_features = {"pixels": features.values}
+    raw_targets = np.array(labels)
+    
+    ds = Dataset.from_tensor_slices((raw_features, raw_targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size)
+    
+        
+    # Return the next batch of data.
+    feature_batch, label_batch = ds.make_one_shot_iterator().get_next()
+    return feature_batch, label_batch
+
+  return _input_fn
+
+def train_dnn_classification_model(
+    learning_rate,
+    steps,
+    batch_size,
+    hidden_units,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+  """Trains a linear classification model for the MNIST digits dataset.
+  
+  In addition to training, this function also prints training progress information,
+  a plot of the training and validation loss over time, and a confusion
+  matrix.
+  
+  Args:
+    learning_rate: An `int`, the learning rate to use.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    training_examples: A `DataFrame` containing the training features.
+    training_targets: A `DataFrame` containing the training labels.
+    validation_examples: A `DataFrame` containing the validation features.
+    validation_targets: A `DataFrame` containing the validation labels.
+      
+  Returns:
+    The trained `LinearClassifier` object.
+  """
+
+  periods = 10
+
+  steps_per_period = steps / periods  
+  # Create the input functions.
+  predict_training_input_fn = create_predict_input_fn(
+    training_examples, training_targets, batch_size)
+  predict_validation_input_fn = create_predict_input_fn(
+    validation_examples, validation_targets, batch_size)
+  training_input_fn = create_training_input_fn(
+    training_examples, training_targets, batch_size)
+  
+  # Create a LinearClassifier object.
+  my_optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  classifier = tf.estimator.DNNClassifier(
+      feature_columns=construct_feature_columns(),
+      hidden_units=hidden_units,
+      n_classes=10,
+      optimizer=my_optimizer,
+      config=tf.estimator.RunConfig(keep_checkpoint_max=1)
+  )
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("LogLoss error (on validation data):")
+  training_errors = []
+  validation_errors = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    classifier.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+  
+    # Take a break and compute probabilities.
+    training_predictions = list(classifier.predict(input_fn=predict_training_input_fn))
+    training_probabilities = np.array([item['probabilities'] for item in training_predictions])
+    training_pred_class_id = np.array([item['class_ids'][0] for item in training_predictions])
+    training_pred_one_hot = tf.keras.utils.to_categorical(training_pred_class_id,10)
+        
+    validation_predictions = list(classifier.predict(input_fn=predict_validation_input_fn))
+    validation_probabilities = np.array([item['probabilities'] for item in validation_predictions])    
+    validation_pred_class_id = np.array([item['class_ids'][0] for item in validation_predictions])
+    validation_pred_one_hot = tf.keras.utils.to_categorical(validation_pred_class_id,10)    
+    
+    # Compute training and validation errors.
+    training_log_loss = metrics.log_loss(training_targets, training_pred_one_hot)
+    validation_log_loss = metrics.log_loss(validation_targets, validation_pred_one_hot)
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, validation_log_loss))
+    # Add the loss metrics from this period to our list.
+    training_errors.append(training_log_loss)
+    validation_errors.append(validation_log_loss)
+  print("Model training finished.")
+  # Remove event files to save disk space.
+  _ = map(os.remove, glob.glob(os.path.join(classifier.model_dir, 'events.out.tfevents*')))
+  
+  # Calculate final predictions (not probabilities, as above).
+  final_predictions = classifier.predict(input_fn=predict_validation_input_fn)
+  final_predictions = np.array([item['class_ids'][0] for item in final_predictions])
+  
+  
+  accuracy = metrics.accuracy_score(validation_targets, final_predictions)
+  print("Final accuracy (on validation data): %0.2f" % accuracy)
+
+  return classifier
+
+
+dnn_classifier = train_dnn_classification_model(
+             learning_rate=0.05,
+             steps=2000,
+             batch_size=100, hidden_units = [128, 128, 128],
+             training_examples=training_examples,
+             training_targets=training_targets,
+             validation_examples=validation_examples,
+             validation_targets=validation_targets)
+
+mnist_test_dataframe = pd.read_csv(
+  "mnist_test.csv",
+  sep=",",
+  header=None)
+
+test_targets, test_examples = parse_labels_and_features(mnist_test_dataframe)
+
+predict_test_input_fn = create_predict_input_fn(
+    test_examples, test_targets, batch_size=100)
+
+test_predictions = dnn_classifier.predict(input_fn=predict_test_input_fn)
+test_predictions = np.array([item['class_ids'][0] for item in test_predictions])
+  
+accuracy = metrics.accuracy_score(test_targets, test_predictions)
+print("Accuracy on test data: %0.2f" % accuracy)
+
+
+single_target = test_targets.head(1)
+single_example = test_examples.head(1)
+print(single_target)
+print(single_example)
+single_test_input_fn = create_predict_input_fn(single_example, single_target, batch_size=1)
+single_prediction = dnn_classifier.predict(input_fn=single_test_input_fn)
+
+print("now we are trying to predict an input which is supposed to be 7:")
+for item in single_prediction:
+    print(item['class_ids'][0])
